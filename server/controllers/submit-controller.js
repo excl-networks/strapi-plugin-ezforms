e94ef9b5b2d5a310e86db706beb53e2f6e62245d
Arix Wap <arix.wap@gmail.com>
2022-08-23 18:17:09 +0800

    fix(captcha): prevent save data and send notification on invalid captcha

--- server/controllers/submit-controller.js
+++ server/controllers/submit-controller.js
@@ -1,52 +1,69 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async index(ctx) {
-    let verification = {}
+    let verification = {};
     // Checks if there is a captcha provider
-    if (!(strapi.config.get('plugin.ezforms.captchaProvider.name') === 'none') && (strapi.config.get('plugin.ezforms.captchaProvider.name'))) {
-      verification = await strapi.plugin('ezforms').service(strapi.config.get('plugin.ezforms.captchaProvider.name')).validate(ctx.request.body.token)
+    if (
+      !(strapi.config.get("plugin.ezforms.captchaProvider.name") === "none") &&
+      strapi.config.get("plugin.ezforms.captchaProvider.name")
+    ) {
+      verification = await strapi
+        .plugin("ezforms")
+        .service(strapi.config.get("plugin.ezforms.captchaProvider.name"))
+        .validate(ctx.request.body.token);
       //throws error if invalid
       if (!verification.valid) {
-        strapi.log.error(verification.error)
+        let errorMessage = "";
+
+        strapi.log.error(verification.error);
         if (verification.code === 500) {
-          ctx.internalServerError("There was an error, check Strapi logs for more details. " + verification.message)
+          ctx.internalServerError(
+            (errorMessage =
+              "There was an error, check Strapi logs for more details. " +
+              verification.message)
+          );
         } else if (verification.code === 400) {
-          ctx.badRequest(verification.message)
+          ctx.badRequest((errorMessage = verification.message));
         } else {
-          ctx.internalServerError("There was an error")
+          ctx.internalServerError((errorMessage = "Captcha invalid"));
         }
+
+        return ctx.internalServerError(errorMessage);
       }
     }
 
     //sends notifications
-    for (const provider of strapi.config.get('plugin.ezforms.notificationProviders')) {
+    for (const provider of strapi.config.get(
+      "plugin.ezforms.notificationProviders"
+    )) {
       if (provider.enabled) {
         try {
-          await strapi.plugin('ezforms').service(provider.name).send(provider.config, ctx.request.body.formData)
+          await strapi
+            .plugin("ezforms")
+            .service(provider.name)
+            .send(provider.config, ctx.request.body.formData);
         } catch (e) {
-          strapi.log.error(e)
-          ctx.internalServerError('A Whoopsie Happened');
+          strapi.log.error(e);
+          ctx.internalServerError("A Whoopsie Happened");
         }
       }
     }
 
     // Adds to DB
-    let parsedScore = verification.score || -1
+    let parsedScore = verification.score || -1;
     try {
-      await strapi.query('plugin::ezforms.submission').create({
-          data: {
-            score: parsedScore,
-            data: ctx.request.body.formData,
-          }
-        }
-      );
+      await strapi.query("plugin::ezforms.submission").create({
+        data: {
+          score: parsedScore,
+          data: ctx.request.body.formData,
+        },
+      });
     } catch (e) {
-      strapi.log.error(e)
-      return ctx.internalServerError('A Whoopsie Happened')
+      strapi.log.error(e);
+      return ctx.internalServerError("A Whoopsie Happened");
     }
 
-    return ctx.body = ctx.request.body.formData;
+    return (ctx.body = ctx.request.body.formData);
   },
-}
-;
+};
