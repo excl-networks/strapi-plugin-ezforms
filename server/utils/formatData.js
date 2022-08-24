73fc863338aa6f0c833227f73bf709a78f6bc5c0
Arix Wap <arix.wap@gmail.com>
2022-08-24 18:09:47 +0800

    fix(formatData utils): add format for string

--- server/utils/formatData.js
+++ server/utils/formatData.js
@@ -1,12 +1,18 @@
 module.exports = function formatData(data) {
-  let message = '';
-  //Loop through data and construct message from data object
-  for (let key in data) {
-    if (typeof data[key] === 'object') {
-      message += `${key}: ${JSON.stringify(data[key], null, 2)}\n`;
-    } else {
-      message += `${key}: ${data[key]}\n`;
+  let message = "";
+
+  if (typeof data === "string") {
+    message = data;
+  } else {
+    // Loop through data and construct message from data object
+    for (let key in data) {
+      if (typeof data[key] === "object") {
+        message += `${key}: ${JSON.stringify(data[key], null, 2)}\n`;
+      } else {
+        message += `${key}: ${data[key]}\n`;
+      }
     }
   }
+
   return message;
-}
+};
