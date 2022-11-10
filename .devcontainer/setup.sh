53f22230f86706298b83c5b4624d955062d601b7
Cameron Paczek <me@cameronpaczek.dev>
2022-11-10 13:46:51 -0800

    update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,7 @@ yarn create strapi-app strapi-wrapper --quickstart --no-run
 cd strapi-wrapper/config
 cp -r /workspaces/strapi-plugin-ezforms/.devcontainer/config/plugins.js .
 mkdir -p /workspaces/strapi-wrapper/src/plugins
-mv /workspaces/strapi-plugin-ezforms/ /workspaces/strapi-wrapper/src/plugins/strapi-plugin-ezforms
+# symlink /workspaces/strapi-plugin-ezforms to /workspaces/strapi-wrapper/src/plugins/strapi-plugin-ezforms
+ln -s /workspaces/strapi-plugin-ezforms /workspaces/strapi-wrapper/src/plugins/strapi-plugin-ezforms 
 cd /workspaces/strapi-wrapper
 yarn develop
