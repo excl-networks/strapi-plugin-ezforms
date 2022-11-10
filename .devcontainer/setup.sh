1558003f4f63f5860ee8627eeaffbb2e1c010a75
Cameron Paczek <me@cameronpaczek.dev>
2022-11-10 13:53:26 -0800

    fix typo in setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 cd /workspaces
 yarn create strapi-app strapi-wrapper --quickstart --no-run
 cd strapi-wrapper/config
-cp -r /workspaces/strapi-plugin-ezforms/.devcontainer/config/plugins.js .
+cp -r /workspaces/strapi-plugin-ezforms/.devcontainer/plugins.js .
 mkdir -p /workspaces/strapi-wrapper/src/plugins
 # symlink /workspaces/strapi-plugin-ezforms to /workspaces/strapi-wrapper/src/plugins/strapi-plugin-ezforms
 ln -s /workspaces/strapi-plugin-ezforms /workspaces/strapi-wrapper/src/plugins/strapi-plugin-ezforms 
