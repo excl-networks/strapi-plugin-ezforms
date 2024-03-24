d6cbd7e072a0a102f29ba7a79b47b79d775a263f
Cameron <github@strixdesigns.com>
2024-03-24 04:14:32 -0700

    add allowUnsafeHtml as message

--- server/services/notification-providers/email.js
+++ server/services/notification-providers/email.js
@@ -8,12 +8,18 @@ module.exports = ({strapi}) => ({
     //loop through the recipients and send an email
     for (let recipient of recipients) {
       try {
-        await strapi.plugins['email'].services.email.send({
+        let sendObj = {
           to: recipient.email,
           from: config.from,
           subject: config.subject ? config.subject : 'New Contact Form Submission',
-          text: message,
-        })
+        }
+        if (config.allowUnsafeHtmlAsMessage) {
+          sendObj.html = message
+        }else{
+          sendObj.text = message
+        }
+
+        await strapi.plugins['email'].services.email.send()
       } catch (e) {
         strapi.log.error(e)
       }
